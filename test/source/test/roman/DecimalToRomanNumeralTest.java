ee273b44901985955ecea4e6578ee714ff3fba49
Dario Campagna <campagna@esteco.com>
2019-11-12 09:29:29 +0100

    We now convert 0 to empty string

--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -1,5 +1,15 @@
 package test.roman;
 
+import dssc.roman.RomanNumeral;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DecimalToRomanNumeralTest {
 
+    @Test
+    void zeroIsEmptyString() {
+        RomanNumeral romanNumeral = new RomanNumeral(0);
+        assertEquals("", romanNumeral.toString());
+    }
 }
